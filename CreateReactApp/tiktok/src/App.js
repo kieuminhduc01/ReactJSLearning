f611d2368d04a302ee168dc8f140269b242e136b
KieuMinhDuc <kieuminhduc01@gmail.com>
2021-10-25 17:19:22 +0700

    two-way binding

--- CreateReactApp/tiktok/src/App.js
+++ CreateReactApp/tiktok/src/App.js
@@ -1,21 +1,36 @@
 import { useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(1);
+  // const [counter, setCounter] = useState(1);
 
-  const handleIncrease = () => {
-    setCounter(counter + 1);
-  };
-  const handleDecrease = () => {
-    setCounter(counter - 1);
-  };
-  return (
-    <div className="App" style={{ padding: 20 }}>
-      <h1>{counter}</h1>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleDecrease}>Decrease</button>
-    </div>
-  );
+  // const handleIncrease = () => {
+  //   setCounter(counter + 1);
+  // };
+  // const handleDecrease = () => {
+  //   setCounter(counter - 1);
+  // };
+  // const handleTrippleIncrease = () => {
+  //   setCounter((a) => a + 1);
+  //   setCounter((a) => a + 1);
+  //   setCounter((a) => a + 1);
+  // };
+  // return (
+  //   <div className="App" style={{ padding: 20 }}>
+  //     <h1>{counter}</h1>
+  //     <button onClick={handleIncrease}>Increase</button>
+  //     <button onClick={handleDecrease}>Decrease</button>
+  //     <button onClick={handleTrippleIncrease}>Tripple Increase</button>
+  //   </div>
+  // );
+  const company=[
+    'Tigcon',
+    'Mobifone'
+  ]
+  function App(){
+    return <div style={"padding:32"}>
+      
+    </div>;
+  }
 }
 
 export default App;
